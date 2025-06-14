bc2c449c10faaf1b8df783871a191aec99d98b8a
gechderib <gechderib@gmail.com>
2025-06-14 13:24:37 +0300

    twilio integration google signing dockerization terms acceptanc

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,13 +5,6 @@ set -a
 source .env
 set +a
 
-# Wait for database to be ready
-echo "Waiting for database..."
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 0.1
-done
-echo "Database is ready!"
-
 # Run migrations
 echo "Running migrations..."
 python manage.py makemigrations
