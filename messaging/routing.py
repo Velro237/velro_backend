1f827f7003b041b1e303d11c37ffd93639aebb17
Velro Admin <velro237@gmail.com>
2025-09-07 01:59:30 +0200

    Update routing.py
    ping route added

--- messaging/routing.py
+++ messaging/routing.py
@@ -9,3 +9,15 @@ def get_websocket_urlpatterns():
     ]
 
 websocket_urlpatterns = get_websocket_urlpatterns() 
+
+from django.urls import re_path
+from . import consumers
+
+def get_websocket_urlpatterns():
+    return [
+        re_path(r'ws/chat/(?P<conversation_id>\d+)/$', consumers.ChatConsumer.as_asgi()),
+        re_path(r'ws/notifications/$', consumers.NotificationConsumer.as_asgi()),
+        re_path(r'ws/ping/$', consumers.PingConsumer.as_asgi()),  # ← add this
+    ]
+
+websocket_urlpatterns = get_websocket_urlpatterns()
